4220b996c53fd6ced8dd5577a09793cc4eb5a2e9
svetlana-hanzha <61321374+svetlana-hanzha@users.noreply.github.com>
2020-05-24 18:59:43 +0300

    Update smart.php

--- projects/3_pulse/mailer/smart.php
+++ projects/3_pulse/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'svetlanamailerphp@gmail.com';                 // Наш логин
-$mail->Password = '1q.2w.3e.';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
