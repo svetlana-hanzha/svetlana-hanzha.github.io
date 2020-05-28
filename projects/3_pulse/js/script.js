8e81444dc6f0c93121b0cb7f73a0e7840cc705a6
svetlana-hanzha <61321374+svetlana-hanzha@users.noreply.github.com>
2020-05-28 17:04:44 +0300

    Update script.js

--- projects/3_pulse/js/script.js
+++ projects/3_pulse/js/script.js
@@ -118,9 +118,9 @@ $(document).ready(function(){
 	$('form').submit(function(e) {
 		e.preventDefault();
 		$.ajax({
-			type: "POST",
-			url: "mailer/smart.php",
-			data: $(this).serialize()
+// 			type: "POST",
+// 			url: "mailer/smart.php",
+// 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
 			$('#consultation, #order').fadeOut();
